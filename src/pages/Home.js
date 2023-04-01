1e4b54a6db32c6e42a3341ac3487125940da8bff
jto-d <jodonnell072@gmail.com>
2023-04-01 13:42:07 -0400

    pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import landingPic from '../assets/landing.jpeg'
 
@@ -17,6 +17,35 @@ const StyledPhoto = styled.div`
     display: flex;
 `
 
+const StyledButtons = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 15px;
+    margin-top: 1rem;
+`
+
+const StyledButton = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem 1.5rem;
+    width: 15%;
+
+
+    background-color: #0058FF;
+    font-weight: 400;
+    letter-spacing: .1em;
+    color: #DAE9FF;
+    cursor: pointer;
+    border-radius: 20px;
+
+    ${props => props.invert && css`
+        background-color: #2B3445;
+        color: white;
+    `}
+
+`
+
 const StyledCircle = styled.div`
     width: 15vh;
     height: 15vh;
@@ -24,7 +53,7 @@ const StyledCircle = styled.div`
     background-color: #DAE9FF;
     margin-left: -4vw;
     margin-top: -4vh;
-    z-index: 4;
+    z-index: 0;
     position: absolute;
     /* margin-bottom: 55vh; */
 `
@@ -32,6 +61,20 @@ const StyledHeroText = styled.div`
     display: flex;
     color: #DAE9FF;
     flex-direction: column;
+    width: 31%;
+    justify-content: center;
+    margin: auto;
+
+    h1 {
+        font-size: 3rem;
+    }
+
+    p {
+        margin-top: .8rem;
+        line-height: 2rem;
+        font-size: 1.5rem;
+        font-weight: 300;
+    }
 
 `
 
@@ -43,6 +86,7 @@ const StyledImg = styled.div`
     border-bottom-left-radius: 15vh;
     width: 60vw;
     background-image: url(${landingPic});
+    z-index: 0;
 `
 
 const Home = () => {
@@ -56,8 +100,13 @@ const Home = () => {
                 Fix your form
             </h1>
             <p>
-                Form Fit analyzes your using computer vision to give detailed insights on correcting your exercise form to minimize 
+                Form Fit analyzes your form using computer vision to give detailed insights on correcting your bench press form to minimize injury and maximize muscle gain.
             </p>
+            <StyledButtons>
+                <StyledButton>Register</StyledButton>
+                <StyledButton invert>Log In</StyledButton>
+            </StyledButtons>
+            
         </StyledHeroText>
         <StyledPhoto>
             <StyledCircle/> 
