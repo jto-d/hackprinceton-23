295119a6e5364dfe7de19d2fdb6f7b8b2af63656
Caple Austin Spence <cspence@dynamic-oit-vapornet100-10-9-133-23.princeton.edu>
2023-04-01 16:01:49 -0400

    monke

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { Route, Routes } from 'react-router-dom'
 function App() {
   return (   
     <div> 
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<Home />} />
 
